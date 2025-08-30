8bf9ea434aec74cf9bbc76c1751bee54226749ca
bsallesp <bsallesp@gmail.com>
2025-08-30 08:32:55 -0400

    pre-frontend factory.

--- MDS.Frontend/app/src/app/app.config.ts
+++ MDS.Frontend/app/src/app/app.config.ts
@@ -1,18 +1,30 @@
-import { ApplicationConfig } from '@angular/core';
-import { providePrimeNG } from 'primeng/config';
-import Lara from '@primeuix/themes/lara';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
-import { provideTranslateService } from '@ngx-translate/core';
-import { provideTranslateHttpLoader } from '@ngx-translate/http-loader';
+import { provideHttpClient, HttpClient } from '@angular/common/http';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { providePrimeNG } from 'primeng/config';
+import Lara from '@primeuix/themes/lara';
+
+export function httpTranslateLoader(http: HttpClient) {
+  return new TranslateHttpLoader(http, 'assets/i18n/', '.json');
+}
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideHttpClient(),
-    provideTranslateService({ fallbackLang: 'pt' }),
-    ...provideTranslateHttpLoader({ prefix: 'assets/i18n/', suffix: '.json' }),
+    importProvidersFrom(
+      TranslateModule.forRoot({
+        defaultLanguage: 'pt',
+        loader: {
+          provide: TranslateLoader,
+          useFactory: httpTranslateLoader,
+          deps: [HttpClient]
+        }
+      })
+    ),
     providePrimeNG({ theme: { preset: Lara, options: { darkModeSelector: false } } })
   ]
 };
